7cfef5ffececcca1fdd57d6caedbcaa9aa108adb
davidaliri <davidaliri95@gmail.com>
2024-11-19 12:01:57 -0500

    Add files via upload

---
+++ basedepreguntas.js
@@ -0,0 +1,77 @@
+basedepreguntas = [
+    {
+
+      pregunta: `1. La oración con adjetivo interrogativo es:`,
+      imagen: ``,
+      respuesta: `	Ellos no supieron qué casaca querías que te trajeran`,
+      distractores: [`	Nadie supo que tenías malas notas`, `Angélica no tiene más que reconocer su delito`, `	Ellos no supieron qué casaca querías que te trajeran`, `	El río que pasa por el pueblo está seco`],
+      ayuda: `También pintó la última cena`,
+      ayudaImg: `https://i.ibb.co/wgZZvKc/obras-de-arte-leonardo-da-vinci-google-498711013-153997319-1024x576.jpg`,
+    },
+    {
+      imagen: ` https://i.ibb.co/pRTkhX3/Captura.png` ,
+      pregunta: ``,
+      respuesta: `Adjetivo indefinido	`,
+      distractores: [`Pronombre indefinido	`, `Adjetivo relativo`, `Adverbio de cantidad	`, `Adjetivo posesivo	`],
+      ayuda: `es lo mismo que 2*2`,
+      ayudaImg: `https://i.ibb.co/k1WgGW2/descarga.jpg`,
+    },
+   
+    {
+      imagen: `` ,
+      pregunta: `3. La oración con adjetivo gentilicio es:`,
+      respuesta: `Los alfajores arequipeños son riquísimos`,
+      distractores: [`Los madrileños trabajan desde temprano`, `Los franciscanos hicieron obras de caridad`, `Llegarán muchos extranjeros del sur de Rusia	`, `Me gustan los jamones de los árabes	`],
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `4. La oración, Ese joven compró estos libros interesantes para aquel niño. Presentaadjetivos:`,
+      respuesta: `Demostrativos`,
+      distractores: [`Posesivos`, `Indefinidos`, `Numerales`, `Relativos`],
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `5. Desde el punto de vista morfológico, el adjetivo es:`,
+      respuesta: `Una categoría gramatical variable`,
+      distractores: [`Una categoría que califica a los sustantivos`, `Determinante del sustantivo	`, `Núcleo del predicativo	`, `Presentador del sustantivo	`],
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `6. La oración con adjetivo gentilicio es:`,
+      respuesta: `El pan francés es riquísimo`,
+      distractores: [`Los franciscanos traerán regalos para el pueblo	`, `Los cervantinos llegarán al Perú`, `Los madrileños harán grandes obras	`, `El boricua alegró la ceremonia	`],
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `7. La oración que presenta un adjetivo determinativo, es:`,
+      respuesta: `Traje media torta para comer`,
+      distractores: [`La muchacha amable trajo pasteles para compartir	`, `Los buenos deseos son importantes para una vida plácida`, `Ninguno tiene lo que busco`, `¿Me compartes la mitad?`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/MMYd2pS/Captura-PNG1.png` ,
+      pregunta: ``,
+      respuesta: `Epíteto	`,
+      distractores: [`Gentilicio	`, `Superlativo	`, `Numeral`, `Indefinido`],
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `9. Sintácticamente, el adjetivo:`,
+      respuesta: `Funciona como modificador del sustantivo`,
+      distractores: [`Es una categoría de inventario abierto	`, `Es una palabra independiente	`, `Jamás podrá convertirse en un sustantivo	`, `Se clasifica en calificativos y determinativos	`],
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `10. El adjetivo con función de núcleo del predicativo es:`,
+      respuesta: `Mis amigos vinieron muy felices de la feria`,
+      distractores: [`Mis amigos estarán en la enorme plaza del pueblo	`, `Ellos parecían cansados de tanto viaje	`, `Estuvieron lejos de mí`, `Nadie supo de los tristes niños	`],
+    }
+
+
+  ];
